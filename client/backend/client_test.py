96b85ca114e3b9ba3249d167063d5a1122aa4c0b
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-10-12 21:51:31 +0200

    * initial structure for all components

---
+++ client/backend/client_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestClientMethods(unittest.TestCase):
+
+    def test_env(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ vote_proxy/backend/vote_proxy_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestVotePRoxyMethods(unittest.TestCase):
+
+    def test_env(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
