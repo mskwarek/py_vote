97f75cadfc8c5df06c910edd98073d5286d57146
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-10-19 20:51:43 +0200

    * added simple endpoints with some tests

--- election_authority/backend/election_authority.py
+++ election_authority/backend/election_authority.py
@@ -1,5 +1,5 @@
 import flask
-
+from flask import jsonify
 
 app = flask.Flask(__name__, instance_relative_config=True)
 class ElectionAuthority():
@@ -8,6 +8,21 @@ class ElectionAuthority():
 
 @app.route('/')
 def index():
+    return "hello"
+
+
+@app.route('/candidates')
+def get_candidates():
+    return jsonify()
+
+
+@app.route('/proxy/ballot/blind', methods=['GET', 'POST'])
+def post_blinded_proxy_ballot():
+    return "hello"
+
+
+@app.route('/proxy/ballot/unblinded', methods=['GET', 'POST'])
+def post_unblinded_proxy_ballot():
     return "hello"
 
 
--- election_authority/backend/test_election_authority.py
+++ election_authority/backend/test_election_authority.py
@@ -1,6 +1,7 @@
 import pytest
 import flask
 import election_authority
+import json
 
 
 @pytest.fixture
@@ -9,15 +10,27 @@ def client():
     client = flask_app.test_client()
     yield client
 
-def test_empty_db(client):
+def test_default_get(client):
     rv = client.get('/')
     assert 200 == rv.status_code
 
-def test_get_candidate_list_endpoint(client):
+def test_rc_get_candidate_list_endpoint(client):
     rv = client.get('/candidates')
     assert 200 == rv.status_code
 
+
+def test_get_non_empty_candidate_list_endpoint(client):
+    rv = client.get('/candidates')
+    data = json.loads(rv.get_data(as_text=True))
+    assert 200 == rv.status_code
+    assert data != None
+
+
 def test_blind_proxy_ballot_endpoint(client):
-    rv = client.get('/proxy/ballot/blind')
+    rv = client.post('/proxy/ballot/blind')
+    assert 200 == rv.status_code
+
+def test_post_unblinded_proxy_ballot_endpoint(client):
+    rv = client.post('/proxy/ballot/unblinded')
     assert 200 == rv.status_code
 
