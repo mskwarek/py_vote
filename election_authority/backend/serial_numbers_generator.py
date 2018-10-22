edf019cfba2b4e2aee3b2030a05aacd2fb8004b0
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-10-22 17:48:47 +0200

    * generate 64-bit serial numbers

--- election_authority/backend/serial_numbers_generator.py
+++ election_authority/backend/serial_numbers_generator.py
@@ -4,4 +4,4 @@ class SerialNumbersgenerator():
 
     @staticmethod
     def generateSerialNumbersList(listSize, serialNumberBitSize):
-        return [random.randint(0, 1) for i in range(0, listSize)]
+        return [random.getrandbits(64) for i in range(0, listSize)]
--- election_authority/backend/test_serial_numbers_generator.py
+++ election_authority/backend/test_serial_numbers_generator.py
@@ -1,7 +1,15 @@
 import pytest
 import serial_numbers_generator
+import sys
 
 
-def test_default_get():
+def test_returened_list_length():
     numbersList = serial_numbers_generator.SerialNumbersgenerator.generateSerialNumbersList(10, 64)
     assert len(numbersList) == 10
+
+
+def test_returned_numbers_bit_length():
+    numbersList = serial_numbers_generator.SerialNumbersgenerator.generateSerialNumbersList(10, 64)
+    for item in numbersList:
+        assert item.bit_length() <= 64
+        assert item.bit_length() > 1
